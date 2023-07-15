3a89f624be9cdacde6ad098319d1528bc225e0c6
ASGJxy <armasjack11@gmail.com>
2023-07-14 23:54:42 -0500

    feat: add api call

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,27 @@
-
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=894be75e1373b9de119cf9a713e53358&hash=486d853d56cba4b67abc43837d45b56b")
+      .then((response) => response.json())
+      .then((data) => setData(data.data.results))
+  }, [])
+  console.log(data)
   return (
     <div className="App">
-      
+      <h1>PruebA</h1>
+      <div className="card">
+        <ul>
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>))}
+        </ul>
+      </div>
     </div>
   );
 }
-
 export default App;
+
+
